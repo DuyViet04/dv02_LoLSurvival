2f1e14f7b633b9f638459dcd03a7ba72211e6c08
DuyViet04 <duyviet130904@gmail.com>
2025-04-29 18:18:37 +0700

    add moving script

---
+++ Assets/_Data/Scripts/Moving.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    [SerializeField] private Rigidbody playerRigid;
+    [SerializeField] private float moveSpeed = 7f;
+
+    private void Update()
+    {
+        this.Move();
+    }
+
+    void Move()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        float zDir = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(xDir, 0, zDir);
+        
+        this.playerRigid.velocity = moveDir * this.moveSpeed;
+    }
+}
