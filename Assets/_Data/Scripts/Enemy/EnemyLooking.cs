cf85ede79780809560faf51c34e4b9ea34284f53
DuyViet04 <duyviet130904@gmail.com>
2025-04-29 23:59:25 +0700

    refactor EnemyLooking.cs

--- Assets/_Data/Scripts/Enemy/EnemyLooking.cs
+++ Assets/_Data/Scripts/Enemy/EnemyLooking.cs
@@ -3,24 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyLooking : MonoBehaviour
+public class EnemyLooking : LookingTarget
 {
-    [SerializeField] private Transform target;
-
     private void Update()
     {
-        this.LookAtTarget();
-    }
-
-    void LookAtTarget()
-    {
-        Vector3 enemyPos = this.transform.parent.position;
-        Vector3 targetPos = this.target.position;
-
-        float x = targetPos.x - enemyPos.x;
-        float z = targetPos.z - enemyPos.z;
-        float angle = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
-
-        this.transform.parent.rotation = Quaternion.Euler(0, angle, 0);
+        this.LookAtTarget(this.transform.parent, this.target.transform);
     }
 }
