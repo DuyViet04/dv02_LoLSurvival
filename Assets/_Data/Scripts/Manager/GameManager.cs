847ac561a8aa37254cc9a837866d280cb575059a
DuyViet04 <duyviet130904@gmail.com>
2025-05-12 08:27:58 +0700

    add reset data when play

--- Assets/_Data/Scripts/Manager/GameManager.cs
+++ Assets/_Data/Scripts/Manager/GameManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private YasuoStats yasuoStats;
+    [SerializeField] private RarityTable rarityTable;
 
     // Start is called before the first frame update
     void Start()
     {
         this.yasuoStats.ResetStats();
+        this.rarityTable.ResetData();
     }
 
     // Update is called once per frame
