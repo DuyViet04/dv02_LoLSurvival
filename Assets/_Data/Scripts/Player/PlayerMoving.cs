56682c7fff6a833a5e16c97e5232bc8cfaeda5a6
DuyViet04 <duyviet130904@gmail.com>
2025-05-16 23:35:07 +0700

    add: rotate to move direction

--- Assets/_Data/Scripts/Player/PlayerMoving.cs
+++ Assets/_Data/Scripts/Player/PlayerMoving.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerMoving : MonoBehaviour
 {
+    [SerializeField] private FindClosestEnemy findClosest;
     [SerializeField] private YasuoStats stats;
     [SerializeField] private Rigidbody playerRigid;
     [SerializeField] private Animator playerAnimator;
+    private Vector3 lastDirection = Vector3.forward;
 
     private void Update()
     {
@@ -17,7 +19,26 @@ public class PlayerMoving : MonoBehaviour
         float zDir = Input.GetAxis("Vertical");
         Vector3 moveDir = new Vector3(xDir, 0, zDir);
 
+        float dis = this.findClosest.GetDistanceToTarget();
+
+        if (dis > 7)
+        {
+            if (moveDir != Vector3.zero)
+            {
+                this.lastDirection = moveDir.normalized;
+                this.transform.parent.rotation = Quaternion.Slerp(this.transform.parent.rotation,
+                    this.GetRotation(this.lastDirection), Time.deltaTime * 5f);
+            }
+        }
+
         this.playerRigid.velocity = moveDir * this.stats.moveSpeed;
         playerAnimator.SetFloat("isRun", this.playerRigid.velocity.magnitude);
+    }
+
+    Quaternion GetRotation(Vector3 moveDir)
+    {
+        float angle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler(0, angle - 70, 0);
+        return rotation;
     }
 }
