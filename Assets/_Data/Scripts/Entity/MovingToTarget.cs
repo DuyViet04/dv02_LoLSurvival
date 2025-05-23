3442fe2b5e3e9599750aa4fea029d3886bb9c264
DuyViet04 <duyviet130904@gmail.com>
2025-05-23 22:55:49 +0700

    refactor: MovingToTarget.cs

--- Assets/_Data/Scripts/Entity/MovingToTarget.cs
+++ Assets/_Data/Scripts/Entity/MovingToTarget.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class MovingToTarget : MonoBehaviour
+public abstract class MovingToTarget : VyesBehaviour
 {
     [SerializeField] protected Transform target;
-    
+
+
+    //Tự động đi về phía target
     protected virtual void MoveToTarget(float moveSpeed)
     {
         this.transform.parent.position =
