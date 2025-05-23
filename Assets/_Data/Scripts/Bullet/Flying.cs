3b8fa33c1743237c33ec78e7aaa0835c9ca241fc
DuyViet04 <duyviet130904@gmail.com>
2025-05-23 23:16:27 +0700

    refactor: Flying.cs

--- Assets/_Data/Scripts/Bullet/Flying.cs
+++ Assets/_Data/Scripts/Bullet/Flying.cs
@@ -1,10 +1,12 @@
-using System;
 using UnityEngine;
 
-public class Flying : MonoBehaviour
+public class Flying : VyesBehaviour
 {
+    private float flySpeed = 5f;
+
+    //Dịch chuyển theo hướng trước mặt
     private void Update()
     {
-        this.transform.parent.Translate(Vector3.forward * 5 * Time.deltaTime);
+        this.transform.parent.Translate(Vector3.forward * this.flySpeed * Time.deltaTime);
     }
 }
