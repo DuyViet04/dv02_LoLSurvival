68be16931c1a2acbad4e18a19ae465a085bc5491
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 14:30:44 +0700

    refactor: EnemyDealingDamage.cs

--- Assets/_Data/Scripts/Enemy/EnemyDealingDamage.cs
+++ Assets/_Data/Scripts/Enemy/EnemyDealingDamage.cs
@@ -1,18 +1,48 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class EnemyDealingDamage : DealingDamage
 {
     [SerializeField] private MainEnemyStats stats;
     [SerializeField] private SOManager soManager;
-
-    private void Awake()
-    {
-        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
-    }
+    [SerializeField] private CapsuleCollider capsuleCollider;
 
     private void FixedUpdate()
     {
         this.damage = this.stats.damage;
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadStats();
+        this.LoadCapsuleCollider();
+    }
+
+    void LoadStats()
+    {
+        this.LoadSoManager();
+
+        if (this.stats != null) return;
+        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        Debug.LogWarning(this.transform.name + ": LoadStats", this.gameObject);
+    }
+
+    void LoadSoManager()
+    {
+        if (this.soManager != null) return;
+        this.soManager = GameObject.FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
+    }
+
+    void LoadCapsuleCollider()
+    {
+        if (this.capsuleCollider != null) return;
+        this.capsuleCollider = this.GetComponent<CapsuleCollider>();
+        this.capsuleCollider.center = new Vector3(0, 0.5f, 0);
+        this.capsuleCollider.radius = 0.5f;
+        this.capsuleCollider.height = 1;
+        Debug.LogWarning(this.transform.name + ": LoadCapsuleCollider", this.gameObject);
     }
 }
