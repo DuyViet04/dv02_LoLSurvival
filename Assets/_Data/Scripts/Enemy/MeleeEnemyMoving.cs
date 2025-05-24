64edde2e810e10d453549c8560fa8744e62008f2
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 23:01:05 +0700

    refactor: MeleeEnemyMoving.cs

--- Assets/_Data/Scripts/Enemy/MeleeEnemyMoving.cs
+++ Assets/_Data/Scripts/Enemy/MeleeEnemyMoving.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class MeleeEnemyMoving : MovingToTarget
@@ -6,13 +5,30 @@ public class MeleeEnemyMoving : MovingToTarget
     [SerializeField] private MainEnemyStats stats;
     [SerializeField] private SOManager soManager;
 
-    private void Awake()
-    {
-        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
-    }
-
     private void Update()
     {
         this.MoveToTarget(this.stats.moveSpeed);
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadStats();
+    }
+
+    void LoadStats()
+    {
+        this.LoadSoManager();
+
+        if (this.stats != null) return;
+        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        Debug.LogWarning(this.transform.name + ": LoadStats", this.gameObject);
+    }
+
+    void LoadSoManager()
+    {
+        if (this.soManager != null) return;
+        this.soManager = GameObject.FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
     }
 }
