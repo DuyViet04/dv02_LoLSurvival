5c4b1fe93933722ee6c8493465d9da914e9038e9
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 22:05:18 +0700

    refactor: EnemyScaleStats.cs

--- Assets/_Data/Scripts/Enemy/EnemyScaleStats.cs
+++ Assets/_Data/Scripts/Enemy/EnemyScaleStats.cs
@@ -1,34 +1,66 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyScaleStats : MonoBehaviour
+public class EnemyScaleStats : VyesBehaviour
 {
     [SerializeField] private MainEnemyStats stats;
-    [SerializeField] private TimeDisplay timeDisplay;
+    [SerializeField] private LevelUp levelUp;
     [SerializeField] private SOManager soManager;
     private MainEnemyStats baseStats;
 
-    private void Awake()
+    private void FixedUpdate()
     {
-        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        float currentLevel = this.levelUp.GetCurrentLevel();
+        this.ScaleByLevel(currentLevel, this.stats, this.baseStats);
+    }
+
+    void ScaleByLevel(float level, MainEnemyStats stat, MainEnemyStats baseStat)
+    {
+        stat.health = baseStat.health * (1 + level * 0.1f);
+        stat.damage = baseStat.damage * (1 + level * 0.1f);
+        stat.moveSpeed = baseStat.moveSpeed * (1 + level * 0.1f);
+        stat.armor = baseStat.armor * (1 + level * 0.1f);
+        stat.magicResistance = baseStat.magicResistance * (1 + level * 0.1f);
+        stat.expValue = baseStat.expValue * (1 + level * 0.1f);
+        stat.goldValue = baseStat.goldValue * (1 + level * 0.1f);
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadStats();
+        this.LoadLevelUp();
+    }
+
+    protected override void ResetValues()
+    {
+        base.ResetValues();
         this.baseStats = this.stats.GetBaseStats();
     }
 
-    private void FixedUpdate()
+    void LoadStats()
     {
-        float time = this.timeDisplay.GetTime();
-        this.ScaleByTime(time, this.stats, this.baseStats);
+        this.LoadSoManager();
+
+        if (this.stats != null) return;
+        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        Debug.LogWarning(this.transform.name + ": LoadStats", this.gameObject);
     }
 
-    void ScaleByTime(float time, MainEnemyStats stat, MainEnemyStats baseStat)
+    void LoadSoManager()
     {
-        stat.health = baseStat.health * (1f + time * 0.01f);
-        stat.damage = baseStat.damage * (1f + time * 0.01f);
-        stat.armor = baseStat.armor * (1f + time * 0.01f);
-        stat.moveSpeed = baseStat.moveSpeed * (1f + time * 0.001f);
-        stat.expValue = baseStat.expValue * (1f + time * 0.01f);
-        stat.goldValue = baseStat.goldValue * (1f + time * 0.01f);
+        if (this.soManager != null) return;
+        this.soManager = GameObject.FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
+    }
+
+    void LoadLevelUp()
+    {
+        if (this.levelUp != null) return;
+        this.levelUp = GameObject.FindObjectOfType<LevelUp>();
+        Debug.LogWarning(this.transform.name + ": LoadLevelUp", this.gameObject);
     }
 }
--- Assets/_Data/Scripts/Utils/VyesBehaviour.cs
+++ Assets/_Data/Scripts/Utils/VyesBehaviour.cs
@@ -8,14 +8,21 @@ public class VyesBehaviour : MonoBehaviour
     protected virtual void Awake()
     {
         this.LoadComponents();
+        this.ResetValues();
     }
 
     protected virtual void Reset()
     {
         this.LoadComponents();
+        this.ResetValues();
     }
 
     protected virtual void LoadComponents()
+    {
+        //For override...
+    }
+
+    protected virtual void ResetValues()
     {
         //For override...
     }
