d5710f5a17656f9ffb73e34ea691746585086096
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 23:02:51 +0700

    refactor: RangeEnemyMoving.cs

--- Assets/_Data/Scripts/Enemy/RangeEnemyMoving.cs
+++ Assets/_Data/Scripts/Enemy/RangeEnemyMoving.cs
@@ -5,16 +5,11 @@ public class RangeEnemyMoving : MovingToTarget
     [SerializeField] private MainEnemyStats stats;
     [SerializeField] private SOManager soManager;
 
-    private void Awake()
-    {
-        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
-    }
-
     private void Update()
     {
         float dis = this.GetDistanceToTarget();
 
-        float moveSpeed = dis < 10 ? 0f : this.stats.moveSpeed;
+        float moveSpeed = dis < 10 ? 0f : this.stats.moveSpeed; // Nếu khoảng cách nhỏ hơn 10 thì dừng lại
         this.MoveToTarget(moveSpeed);
     }
 
@@ -22,5 +17,27 @@ public class RangeEnemyMoving : MovingToTarget
     {
         float dis = Vector3.Distance(this.target.position, this.transform.parent.position);
         return dis;
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadStats();
+    }
+
+    void LoadStats()
+    {
+        this.LoadSoManager();
+
+        if (this.stats != null) return;
+        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        Debug.LogWarning(this.transform.name + ": LoadStats", this.gameObject);
+    }
+
+    void LoadSoManager()
+    {
+        if (this.soManager != null) return;
+        this.soManager = GameObject.FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
     }
 }
