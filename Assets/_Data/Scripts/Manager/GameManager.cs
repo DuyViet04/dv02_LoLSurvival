c33ae7b44510cf0d89d740f662a752dee395a5b1
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 23:39:33 +0700

    refactor: GameManager.cs

--- Assets/_Data/Scripts/Manager/GameManager.cs
+++ Assets/_Data/Scripts/Manager/GameManager.cs
@@ -1,15 +1,42 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : VyesBehaviour
 {
     [SerializeField] private SOManager soManager;
     [SerializeField] private YasuoStats yasuoStats;
 
-    private void Awake()
+    protected override void LoadComponents()
     {
+        base.LoadComponents();
+        this.LoadYasuoStats();
+        this.LoadSOManager();
+    }
+
+    //Reset giá trị của các SO về mặc định
+    protected override void ResetValues()
+    {
+        base.ResetValues();
         this.ResetStats();
+    }
+
+    void LoadYasuoStats()
+    {
+        if (this.yasuoStats != null) return;
+        string[] guids =
+            AssetDatabase.FindAssets("t:YasuoStats", new string[] { "Assets/_Data/Scripts/Stat/Character/SO" });
+        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        this.yasuoStats = AssetDatabase.LoadAssetAtPath<YasuoStats>(path);
+        Debug.LogWarning(this.transform.name + ": LoadYasuoStats", this.gameObject);
+    }
+
+    void LoadSOManager()
+    {
+        if (this.soManager != null) return;
+        this.soManager = FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
     }
 
     void ResetStats()
