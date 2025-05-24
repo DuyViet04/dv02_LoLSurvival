3588810adacb3c4fff83cbcf2dcfa2eb5b839b06
DuyViet04 <duyviet130904@gmail.com>
2025-05-24 22:46:49 +0700

    refactor: EnemyTakingDamage.cs

--- Assets/_Data/Scripts/Enemy/EnemyTakingDamage.cs
+++ Assets/_Data/Scripts/Enemy/EnemyTakingDamage.cs
@@ -1,18 +1,11 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class EnemyTakingDamage : TakingDamage
 {
     [SerializeField] private MainEnemyStats stats;
     [SerializeField] private GoldDisplay goldDisplay;
     [SerializeField] private SOManager soManager;
-
-    private void Awake()
-    {
-        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
-    }
 
     private void Start()
     {
@@ -25,6 +18,7 @@ public class EnemyTakingDamage : TakingDamage
         this.maxHp = this.stats.health;
     }
 
+    //Spawn quái nhỏ nếu là 2 loại dưới
     protected override void Despawn()
     {
         switch (this.transform.parent.name)
@@ -45,12 +39,14 @@ public class EnemyTakingDamage : TakingDamage
         EnemySpawner.Instance.Despawn(this.transform.parent);
     }
 
+    //Tạo exp khi chết
     void CreateExp()
     {
         Transform exp = ExpSpawner.Instance.Spawn("Exp", this.transform.parent.position, Quaternion.identity);
         exp.GetComponentInChildren<ExpBehaviour>().SetExpValue(this.stats.expValue);
     }
 
+    //Khởi tạo tại currentHp bằng giá trị hp mới nhất
     void ResetStats()
     {
         this.currentHp = this.stats.health;
@@ -62,5 +58,35 @@ public class EnemyTakingDamage : TakingDamage
         {
             other.GetComponent<YasuoWeapon>().DealDamage(this.transform);
         }
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadStats();
+        this.LoadGoldDisplay();
+    }
+
+    void LoadStats()
+    {
+        this.LoadSoManager();
+
+        if (this.stats != null) return;
+        this.stats = this.soManager.GetStatsByType(this.transform.parent.name);
+        Debug.LogWarning(this.transform.name + ": LoadStats", this.gameObject);
+    }
+
+    void LoadSoManager()
+    {
+        if (this.soManager != null) return;
+        this.soManager = GameObject.FindObjectOfType<SOManager>();
+        Debug.LogWarning(this.transform.name + ": LoadSOManager", this.gameObject);
+    }
+
+    void LoadGoldDisplay()
+    {
+        if (this.goldDisplay != null) return;
+        this.goldDisplay = FindObjectOfType<GoldDisplay>();
+        Debug.LogWarning(this.transform.name + ": LoadGoldDisplay", this.gameObject);
     }
 }
