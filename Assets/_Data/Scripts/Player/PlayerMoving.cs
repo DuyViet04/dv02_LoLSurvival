a14bbf25a48a8f7820ea30e6ba5a779293be5c95
DuyViet04 <duyviet130904@gmail.com>
2025-05-25 18:21:44 +0700

    refactor: PlayerMoving.cs

--- Assets/_Data/Scripts/Player/PlayerMoving.cs
+++ Assets/_Data/Scripts/Player/PlayerMoving.cs
@@ -1,8 +1,9 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerMoving : MonoBehaviour
+public class PlayerMoving : VyesBehaviour
 {
     [SerializeField] private YasuoStats yasuoStats;
     [SerializeField] private FindClosestEnemy findClosest;
@@ -42,5 +43,44 @@ public class PlayerMoving : MonoBehaviour
         float angle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.Euler(0, angle - 70, 0);
         return rotation;
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadYasuoStats();
+        this.LoadFindClosest();
+        this.LoadAnimator();
+        this.LoadRigidbody();
+    }
+
+    void LoadYasuoStats()
+    {
+        if (this.yasuoStats != null) return;
+        string[] guids = AssetDatabase.FindAssets("t:YasuoStats", new[] { "Assets/_Data/Scripts/Stat/Character/SO" });
+        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        this.yasuoStats = AssetDatabase.LoadAssetAtPath<YasuoStats>(path);
+        Debug.LogWarning(this.transform.name + ": LoadYasuoStats", this.gameObject);
+    }
+
+    void LoadFindClosest()
+    {
+        if (this.findClosest != null) return;
+        this.findClosest = FindObjectOfType<FindClosestEnemy>();
+        Debug.LogWarning(this.transform.name + ": LoadFindClosest", this.gameObject);
+    }
+
+    void LoadAnimator()
+    {
+        if (this.playerAnimator != null) return;
+        this.playerAnimator = this.transform.parent.GetComponentInChildren<Animator>();
+        Debug.LogWarning(this.transform.name + ": LoadAnimator", this.gameObject);
+    }
+
+    void LoadRigidbody()
+    {
+        if (this.playerRigid != null) return;
+        this.playerRigid = this.transform.parent.GetComponent<Rigidbody>();
+        Debug.LogWarning(this.transform.name + ": LoadRigidbody", this.gameObject);
     }
 }
