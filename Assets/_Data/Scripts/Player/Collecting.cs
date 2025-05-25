3006ebd374f2f5897a48c154f442e7992ddbe156
DuyViet04 <duyviet130904@gmail.com>
2025-05-25 17:59:02 +0700

    refactor: Collecting.cs

--- Assets/_Data/Scripts/Player/Collecting.cs
+++ Assets/_Data/Scripts/Player/Collecting.cs
@@ -1,11 +1,13 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 
-public class Collecting : MonoBehaviour
+[RequireComponent(typeof(CapsuleCollider))]
+public class Collecting : VyesBehaviour
 {
     [SerializeField] private YasuoStats yasuoStats;
     [SerializeField] private CapsuleCollider pickUpCollider;
-    
+
     private void FixedUpdate()
     {
         this.pickUpCollider.radius = this.yasuoStats.pickUpRange;
@@ -19,5 +21,28 @@ public class Collecting : MonoBehaviour
         {
             other.GetComponent<ExpBehaviour>().StartMove();
         }
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadYasuoStats();
+        this.LoadPickUpCollider();
+    }
+
+    void LoadYasuoStats()
+    {
+        if (this.yasuoStats != null) return;
+        string[] guids = AssetDatabase.FindAssets("t:YasuoStats", new[] { "Assets/_Data/Scripts/Stat/Character/SO" });
+        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        this.yasuoStats = AssetDatabase.LoadAssetAtPath<YasuoStats>(path);
+        Debug.LogWarning(this.transform.name + ": LoadYasuoStats", this.gameObject);
+    }
+
+    void LoadPickUpCollider()
+    {
+        if (this.pickUpCollider != null) return;
+        this.pickUpCollider = this.GetComponent<CapsuleCollider>();
+        Debug.LogWarning(this.transform.name + ": LoadPickUpCollider", this.pickUpCollider);
     }
 }
