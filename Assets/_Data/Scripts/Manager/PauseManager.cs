7abfa7b5b41b4258050f7eea4be1b6117afeb29b
DuyViet04 <duyviet130904@gmail.com>
2025-06-03 23:51:33 +0700

    add: show item when pause

--- Assets/_Data/Scripts/Manager/PauseManager.cs
+++ Assets/_Data/Scripts/Manager/PauseManager.cs
@@ -1,16 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : VyesBehaviour
 {
     [SerializeField] private GameObject levelUpPanel;
     [SerializeField] private GameObject pauseGamePanel;
     [SerializeField] private GameObject shopPanel;
+    [SerializeField] private GameObject itemSlot;
+    private List<Image> itemSlots;
+    private List<Sprite> itemSprites;
     private bool isLevelUp;
     private bool isShopping;
 
     protected override void Awake()
     {
         base.Awake();
+        this.itemSlots = this.LoadItemSlots();
         this.levelUpPanel.SetActive(false);
         this.pauseGamePanel.SetActive(false);
         this.shopPanel.SetActive(false);
@@ -37,6 +43,8 @@ public class PauseManager : VyesBehaviour
         Time.timeScale = 0;
         StatsDisplay.Instance.UpdateMainData();
         StatsDisplay.Instance.UpdateSecondData();
+
+        this.UpdateItem();
     }
 
     public void ContinueGame()
@@ -57,12 +65,25 @@ public class PauseManager : VyesBehaviour
         }
     }
 
+    void UpdateItem()
+    {
+        List<Sprite> itemSlots = ShopManager.Instance.GetLastItem();
+        for (int i = 0; i < itemSlots.Count; i++)
+        {
+            if (i < this.itemSlots.Count)
+            {
+                this.itemSlots[i].sprite = itemSlots[i];
+            }
+        }
+    }
+
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadLevelUpPanel();
         this.LoadGamePausePanel();
         this.LoadShopPanel();
+        this.LoadItemSlot();
     }
 
     void LoadLevelUpPanel()
@@ -84,5 +105,24 @@ public class PauseManager : VyesBehaviour
         if (this.shopPanel != null) return;
         this.shopPanel = GameObject.Find("ShopPanel");
         Debug.LogWarning(this.transform.name + ": LoadShopPanel", this.gameObject);
+    }
+
+    List<Image> LoadItemSlots()
+    {
+        List<Image> list = new List<Image>();
+        foreach (Transform item in this.itemSlot.transform)
+        {
+            Image itemImg = item.GetComponent<Image>();
+            list.Add(itemImg);
+        }
+
+        return list;
+    }
+
+    void LoadItemSlot()
+    {
+        if (this.itemSlot != null) return;
+        this.itemSlot = GameObject.Find("ItemSlot");
+        Debug.LogWarning(this.transform.name + ": LoadItemSlot", this.gameObject);
     }
 }
