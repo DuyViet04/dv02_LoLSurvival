5aa3c4dd18f60474b63bab23938fdd2271cb94cc
DuyViet04 <duyviet130904@gmail.com>
2025-06-08 12:31:53 +0700

    fix: dont back to main menu when die

--- Assets/_Data/Scripts/Manager/GameoverManager.cs
+++ Assets/_Data/Scripts/Manager/GameoverManager.cs
@@ -3,12 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameoverManager : VyesSingleton<GameoverManager>
 {
+    [SerializeField] private Button exitButton;
+
     public void Exit()
     {
-        SceneLevelManager.Instance.GoToScene("MainMenu");
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            this.LoadExitButton();
+        }
+
+        if (this.exitButton != null)
+        {
+            this.exitButton.onClick.RemoveAllListeners();
+            this.exitButton.onClick.AddListener(() => SceneLevelManager.Instance.GoToScene("MainMenu"));
+        }
+    }
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadExitButton();
+    }
+
+    void LoadExitButton()
+    {
+        if (this.exitButton != null) return;
+        this.exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        Debug.LogWarning(this.transform.name + ": LoadExitButton", this.gameObject);
     }
 }
