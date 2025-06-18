a25f86745ce5b4e47795f342415804c99aa923e9
DuyViet04 <duyviet130904@gmail.com>
2025-06-18 08:41:23 +0700

    update: add tutorial

--- Assets/_Data/Scripts/Player/PlayerMoving.cs
+++ Assets/_Data/Scripts/Player/PlayerMoving.cs
@@ -5,6 +5,13 @@ public class PlayerMoving : VyesBehaviour
     [SerializeField] private YasuoStats yasuoStats;
     [SerializeField] private Rigidbody playerRigid;
     [SerializeField] private Animator playerAnimator;
+    [SerializeField] private GameObject tutorial;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        this.tutorial.SetActive(true);
+    }
 
     private void Update()
     {
@@ -14,9 +21,12 @@ public class PlayerMoving : VyesBehaviour
     void Move()
     {
         // Lấy giá trị từ bàn phím 
-        float xDir = Input.GetAxis("Horizontal"); 
+        float xDir = Input.GetAxis("Horizontal");
         float zDir = Input.GetAxis("Vertical");
         Vector3 moveDir = new Vector3(xDir, 0, zDir); // Tạo vector di chuyển từ các giá trị bàn phím
+
+        if (moveDir == Vector3.zero) return;
+        this.tutorial.SetActive(false);
 
         this.playerRigid.velocity = moveDir * this.yasuoStats.moveSpeed;
         this.playerAnimator.SetFloat("isRun", this.playerRigid.velocity.magnitude);
@@ -28,6 +38,7 @@ public class PlayerMoving : VyesBehaviour
         this.LoadYasuoStats();
         this.LoadAnimator();
         this.LoadRigidbody();
+        this.LoadTutorial();
     }
 
     void LoadYasuoStats()
@@ -49,5 +60,12 @@ public class PlayerMoving : VyesBehaviour
         if (this.playerRigid != null) return;
         this.playerRigid = this.transform.parent.GetComponent<Rigidbody>();
         Debug.LogWarning(this.transform.name + ": LoadRigidbody", this.gameObject);
+    }
+
+    void LoadTutorial()
+    {
+        if (this.tutorial != null) return;
+        this.tutorial = GameObject.Find("Tutorial");
+        Debug.LogWarning(this.transform.name + ": LoadTutorial", this.gameObject);
     }
 }
